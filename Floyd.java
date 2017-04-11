f7354fbb178dc01226a5634e3660633605bdb1b4
asd1110 <dharani830@gmail.com>
2017-04-11 17:45:24 +0530

    Add files via upload

---
+++ Floyd.java
@@ -0,0 +1,22 @@
+class Floyd{
+public static void main(String args[]){
+int i,j,c=0;
+for(i=0;i<=5;i++){
+	c=i;
+	 for(j=0;j<i;j++){
+	if(c%2==0){
+	System.out.print("*"+" ");	
+		}
+		else{
+		System.out.print("*"+" ");	
+		}
+		c++;
+			}
+		System.out.println();
+	}
+
+
+
+
+}
+}
